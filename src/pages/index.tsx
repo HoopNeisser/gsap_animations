1acb2c2644304acbe542652a8fd601a167cb76c7
Agustin Ezequiel Astudillo <agustin.astudillo97@outlook.es>
2023-10-09 23:09:03 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Prueba from "@/components/prueba/Prueba"
+export { getStaticProps } from "@/tools/getStaticProps";
+
+export default function Home() {
+  return (
+    <Prueba></Prueba>
+  )
+}
